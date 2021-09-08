1971095500a0377619fdae6a090e4d3f2ffdcf08
Rodrigo <rodrigovictoralcorta@gmail.com>
2021-09-08 18:08:18 -0300

    FIX: Deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/random-emoji/' : '/'
+	publicPath: process.env.VUE_APP_PUBLIC_PATH ?? (process.env.NODE_ENV === 'production' ? '/random-emoji/' : '/')
 };
