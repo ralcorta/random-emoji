e1c94ae5d255a9b898d351330bac2f1765bf6b00
Rodrigo <rodrigovictoralcorta@gmail.com>
2021-09-14 11:05:48 -0300

    ADD: Env ghp

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/random-emoji/' : '/'
+	publicPath: '/' //process.env.NODE_ENV === 'production' ? '/random-emoji/' : '/'
 };
